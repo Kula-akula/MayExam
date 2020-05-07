e90035c7c2a9e28e9071ba94971938a428448dd9
Kula <1kulanbek@gmail.com>
2020-05-07 16:39:50 +0600

    start

---
+++ src/main/java/it/academy/MayExam/MayExamApplication.java
@@ -0,0 +1,13 @@
+package it.academy.MayExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/academy/MayExam/MayExamApplicationTests.java
@@ -0,0 +1,13 @@
+package it.academy.MayExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
